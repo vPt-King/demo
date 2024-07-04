dc57b60bfab90217252dd976c9dd8018226ad995
Thanh <vpthanh1302@gmail.com>
2024-07-04 08:19:12 +0700

    using template

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,24 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
-	@GetMapping("/")
-	public String helloWorld()
-	{
-		return "hello world";
-	}
-
-	@GetMapping("/about")
-	public String aboutPage()
-	{
-		return "about";
-	}
-
-
-	@GetMapping("/28")
-	public String test27()
-	{
-		return "this is test page 28";
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
